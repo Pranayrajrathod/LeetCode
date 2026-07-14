93d4bd8a1ee91bfb8a8c3154b8a650ee7babd247
Pranayrajrathod <151193363+Pranayrajrathod@users.noreply.github.com>
2026-07-15 02:07:25 +0530

    Time: 13 ms (6.21%), Space: 19.6 MB (12.59%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -6,41 +6,30 @@ class Solution:
         m=len(grid[0])
         mxans=0
         dir=[[-1,0],[0,-1],[1,0],[0,1]]
-        visited=[[0 for i in range (m)] for i in range (n)]
+        # visited=[[0 for i in range (m)] for i in range (n)]
         ans=[[float("inf") for i in range (m)] for i in range (n)]
         gc=0
 
         for i in range (n):
             for j in range(m):
                 if(grid[i][j]==2):
-                    q.put([i,j])
+                    q.put([i,j,0])
                     ans[i][j]=0
                 if(grid[i][j]==1):
                     gc+=1
         if(gc==0):
             return 0
-
         while(q.qsize()>0):
-            [i,j]=q.get()
-            if(visited[i][j]==1):
-                continue
-            if(grid[i][j]==1):
-                grid[i][j]=2
-                gc-=1
-            visited[i][j]=1
-            if(gc==0):
-                break
+            [i,j,time]=q.get()
+            grid[i][j]=2
             for x,y in dir:
                 if(i+x<0 or j+y<0 or i+x>=n or j+y>=m or grid[i+x][j+y]==0):
                     continue
-                if(visited[i+x][j+y]):
-                    continue
-                q.put([i+x,j+y])
-                ans[i+x][j+y]=min(ans[i+x][j+y],ans[i][j]+1)
-                mxans=max(ans[i+x][j+y],mxans)
-        
-        for i in range (n):
-            for j in range (m):
-                if(grid[i][j]==1):
-                    return -1
+                if(grid[i+x][j+y]==1):    
+                    q.put([i+x,j+y,time+1])
+                    mxans=max(time+1,mxans)
+                    grid[i+x][j+y]=2
+                    gc-=1
+        if(gc!=0):
+            return -1
         return mxans
