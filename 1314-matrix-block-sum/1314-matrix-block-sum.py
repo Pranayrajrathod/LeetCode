6379300075d2dd9e78645ad86d58829701b08576
Pranayrajrathod <151193363+Pranayrajrathod@users.noreply.github.com>
2026-09-21 22:02:02 +0530

    Time: 28 ms (16.96%), Space: 20.9 MB (6.52%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -2,17 +2,20 @@ class Solution:
     def matrixBlockSum(self, mat: list[list[int]], k: int) -> list[list[int]]:
         m=len(mat)
         n=len(mat[0])
-        ans=[[0 for i in range (n)] for i in range (m)]
-
+        ps=[[0 for i in range (n+1)] for i in range (m+1)]
+        
+        for i in range (1,m+1):
+            for j in range (1,n+1):
+                ps[i][j]=ps[i-1][j]+ps[i][j-1]-ps[i-1][j-1]+mat[i-1][j-1]
+        
+        ans=[[0 for i in range(n)] for j in range (m)]
+        print(ps)
         for i in range (m):
             for j in range (n):
-                cs=0
-                for p in range (i-k,i+k+1):
-                    if(p<0 or p>=m):
-                        continue
-                    for q in range (j-k,j+k+1):
-                        if(q<0 or q>=n):
-                            continue
-                        cs+=mat[p][q]
-                ans[i][j]=cs
+                r1=max(0,i-k) 
+                c1=max(0,j-k) 
+                r2=min(m,i+k+1) 
+                c2=min(n,j+k+1)
+                print(i,j,ps[max(0,i-k)][max(0,j-k)])
+                ans[i][j]=ps[r2][c2]-ps[r1][c2]-ps[r2][c1]+ps[max(0,i-k)][max(0,j-k)]
         return ans
